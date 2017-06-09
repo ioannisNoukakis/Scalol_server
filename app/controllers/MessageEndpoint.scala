9376d45f6681bfae7176cc753959e469b9e01625
Ioannis Noukakis <ioannis.noukakis@heig-vd.ch>
2017-06-09 10:44:50 +0200

    added partial functionning websocket endpoint. Needs to be bounded with DB.

--- app/controllers/MessageEndpoint.scala
+++ app/controllers/MessageEndpoint.scala
@@ -8,13 +8,12 @@ import akka.actor.ActorSystem
 import akka.stream.Materializer
 import com.google.inject.Inject
 import models._
-import pdi.jwt.{JwtAlgorithm, JwtJson}
 import pdi.jwt.exceptions.JwtLengthException
-import play.api.mvc._
-import play.api.libs.streams._
-import services.{AuthService, MessageService, UserService}
 import play.api.libs.concurrent.Execution.Implicits._
-import play.api.libs.json.{JsError, JsObject, Json}
+import play.api.libs.json.{JsError, Json}
+import play.api.libs.streams._
+import play.api.mvc._
+import services.{AuthService, MessageService, UserService}
 
 import scala.concurrent.{Await, Future}
 
@@ -22,7 +21,7 @@ import scala.concurrent.{Await, Future}
   * Created by durza9390 on 28.05.2017.
   */
 @Singleton
-class MessageEndpoint @Inject()(implicit MessageDAO: MessageService, UserDAO: UserService, authService: AuthService, system: ActorSystem, materializer: Materializer) extends Controller  {
+class MessageEndpoint @Inject()(implicit MessageDAO: MessageService, UserDAO: UserService, authService: AuthService, system: ActorSystem, materializer: Materializer) extends Controller {
 
   import models.MessageFrom.messageWrites
   import models.MessageTo.messageReads
@@ -37,7 +36,9 @@ class MessageEndpoint @Inject()(implicit MessageDAO: MessageService, UserDAO: Us
         UserDAO.findByUserName(to_username).flatMap(u => {
           MessageDAO.isUserBlocked(request.userSession.user_id, u.id.get).flatMap(blocked => blocked match {
             case Some(x) => x.user_blocked match {
-              case true => Future {Forbidden(Json.obj("cause" -> "This user has blocked you."))}
+              case true => Future {
+                Forbidden(Json.obj("cause" -> "This user has blocked you."))
+              }
               case false => MessageDAO.insert(Message(tmpM.content, false, false, new Date(Calendar.getInstance().getTime().getTime), request.userSession.user_id, u.id.get, None))
                 .map(_ => {
                   Ok(Json.obj("state" -> "ok"))
@@ -52,16 +53,20 @@ class MessageEndpoint @Inject()(implicit MessageDAO: MessageService, UserDAO: Us
     )
   }
 
-  def getMessages(from_username: String) = UserAction.async { implicit request =>
+  def getMessagesFrom(from_username: String) = UserAction.async { implicit request =>
     UserDAO.findByUserName(from_username).flatMap(u => {
       MessageDAO.getLastMessages(request.userSession.user_id, u.id.get).map(r => Ok(Json.toJson(r.map(m => MessageFrom(u.username, m.content, m.date.toString, m.viewed, m.user_blocked)))))
     })
       .recover { case cause => NotFound(Json.obj("reason" -> cause.getMessage)) }
   }
 
+  def getMessages() = UserAction.async { implicit request =>
+
+  }
+
   def blockUser(username: String) = UserAction.async { implicit request =>
     UserDAO.findByUserName(username).flatMap(u => {
-      MessageDAO.updateBlockFromUser(request.userSession.user_id, u.id.get, true).map( _ => {
+      MessageDAO.updateBlockFromUser(request.userSession.user_id, u.id.get, true).map(_ => {
         Ok(Json.obj("status" -> "user blocked."))
       })
     })
@@ -70,7 +75,7 @@ class MessageEndpoint @Inject()(implicit MessageDAO: MessageService, UserDAO: Us
 
   def unblockUser(username: String) = UserAction.async { implicit request =>
     UserDAO.findByUserName(username).flatMap(u => {
-      MessageDAO.updateBlockFromUser(request.userSession.user_id, u.id.get, false).map( _ => {
+      MessageDAO.updateBlockFromUser(request.userSession.user_id, u.id.get, false).map(_ => {
         Ok(Json.obj("status" -> "user unblocked."))
       })
     })
@@ -79,17 +84,24 @@ class MessageEndpoint @Inject()(implicit MessageDAO: MessageService, UserDAO: Us
 
   def markAsRead(to_username: String) = UserAction.async { implicit request =>
     UserDAO.findByUserName(to_username).flatMap(u => {
-      MessageDAO.updateViewed(request.userSession.user_id, u.id.get).map( _ => {
+      MessageDAO.updateViewed(request.userSession.user_id, u.id.get).map(_ => {
         Ok(Json.obj("status" -> "Messages marked as read."))
       })
     })
       .recover { case cause => NotFound(Json.obj("reason" -> cause.getMessage)) }
   }
 
-  def chat = WebSocket.accept[String, String] { request =>
-    val t = authService.verifyToken(request.headers.get("auth").get).map(u => {
-      ActorFlow.actorRef(out => ChatRoom.props(out, u.username, request.headers.get("to").get))
-    })
-    Await.result(t, scala.concurrent.duration.Duration.Inf)
+  def chat(token: Option[String], to: Option[String]) = WebSocket.accept[String, String] { request =>
+    try {
+      val t = authService.verifyToken(token.get).map(u => {
+        ActorFlow.actorRef(out => ChatRoom.props(out, u.username, to.get))
+      })
+      Await.result(t, scala.concurrent.duration.Duration.Inf)
+    } catch {
+      case _: NoSuchElementException => ActorFlow.actorRef(out => ErrorMessageActor.props(out, "Missing auth."))
+      case _: JwtLengthException => ActorFlow.actorRef(out => ErrorMessageActor.props(out, "Invalid auth."))
+      case _: UnsupportedOperationException => ActorFlow.actorRef(out => ErrorMessageActor.props(out, "Invalid auth."))
+      case cause => println(cause); ActorFlow.actorRef(out => ErrorMessageActor.props(out, cause.getMessage))
+    }
   }
 }
--- app/models/ChatRoom.scala
+++ app/models/ChatRoom.scala
@@ -1,6 +1,6 @@
 package models
 
-import akka.actor.{Actor, ActorRef, Props}
+import akka.actor.{Actor, ActorRef, PoisonPill, Props}
 
 import scala.collection.mutable
 
@@ -13,14 +13,17 @@ object ChatRoom {
   def props(out: ActorRef, user: String, to: String) = Props(new ChatRoom(user, to, out))
 }
 
-case class ChatRoom(from: String, to: String, out: ActorRef) extends Actor {
+case class ChatRoom (from: String, to: String, out: ActorRef) extends Actor {
   override def preStart() = {
     super.preStart()
     ChatRoom.queue += this
   }
 
   def receive = {
-    case msg: String => ChatRoom.queue.filter(_.from == to).foreach(_.sender() ! Message(from, to, msg))
+    case msg: String if msg == "check" => out ! "ok"
+    case msg: String => {
+      ChatRoom.queue.filter(_.from == to).foreach(_.sender() ! Message(from, to, msg))
+    }
     case msg: Message => {
       println("sending message from:" + msg.from + " to:" + msg.to + " (" + msg.msg + "")
       ChatRoom.queue.filter(_.to == from).foreach(_.out ! msg.msg)
@@ -33,4 +36,22 @@ case class ChatRoom(from: String, to: String, out: ActorRef) extends Actor {
 
   case class Message(from: String, to: String, msg: String)
 
+}
+
+object ErrorMessageActor{
+  def props(out: ActorRef, msg: String) = Props(new ErrorMessageActor(msg, out))
+}
+
+case class ErrorMessageActor(msg: String, out: ActorRef) extends Actor {
+  override def receive: Receive = {
+    case _: String => {
+      out ! msg
+      self ! PoisonPill
+    }
+  }
+
+  def sendError() = {
+    out ! msg
+    self ! PoisonPill
+  }
 }
